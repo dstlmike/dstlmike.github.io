3573f35b54c5ff7332b10cce51312962ccbd09eb
dstlmike <17973597+dstlmike@users.noreply.github.com>
2022-08-07 21:10:51 -0400

    Update main.js

--- partials/main.js
+++ partials/main.js
@@ -54,12 +54,12 @@ var alternatives = [
 ];
 
 var inputField = document.getElementById("input");
-var buttonField = document.getElementById("submit");
+var buttonField = document.getElementById("submit").click();
 // Execute a function when the user presses a key on the keyboard
 inputField.addEventListener("keypress", function(event) {
 
   // If the user presses the "Enter" key on the keyboard
-  if (event.key === "Enter" || buttonField && (inputField.value) { // || (buttonField.value == "submit" && inputField.value)) { // && input !== "Write something...") {
+  if (event.key === "Enter" || buttonField && (inputField) { // || (buttonField.value == "submit" && inputField.value)) { // && input !== "Write something...") {
     //buttonField =
      let input = inputField.value;
 
