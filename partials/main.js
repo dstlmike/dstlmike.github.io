3659aa09e6f193b6e4b15a78771b4841b6116a41
dstlmike <17973597+dstlmike@users.noreply.github.com>
2022-08-08 23:31:33 -0400

    Update main.js

--- partials/main.js
+++ partials/main.js
@@ -71,6 +71,7 @@ inputField.addEventListener("keypress", function(event) {
     // Cancel the default action, if needed
     event.preventDefault();
    // Trigger the button element with a click
+   
 }
 
 });
