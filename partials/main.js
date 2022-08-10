6ac39e3536787df398b353a7d6ca8fe094a01939
dstlmike <17973597+dstlmike@users.noreply.github.com>
2022-08-10 03:10:46 -0400

    Update main.js

--- partials/main.js
+++ partials/main.js
@@ -154,7 +154,7 @@ function addChatEntry(input, product) {
   let userDiv = document.createElement("div");
   userDiv.id = "user";
   userDiv.className = "user response";
-  userDiv.innerHTML = `User<br><span>${input}</span>`;
+  userDiv.innerHTML = `User\n<span>${input}</span>`;
   
   messagesContainer.appendChild(userDiv);
 messagesContainer.scrollTop = messagesContainer.scrollHeight - messagesContainer.clientHeight; 
@@ -173,13 +173,13 @@ botText.innerText = " ";
   messagesContainer.appendChild(botDiv);
  
   setTimeout(() => {
-  botText.innerText = "Bot<br>Typing...";
+  botText.innerText = "Bot\nTyping...";
 
   messagesContainer.scrollTop =
     messagesContainer.scrollHeight - messagesContainer.clientHeight;
   
   setTimeout(() => {
-    botText.innerText = `Bot<br>${product}`;
+    botText.innerText = `Bot\n${product}`;
 messagesContainer.scrollTop = messagesContainer.scrollHeight - messagesContainer.clientHeight;
 
   }, 5000);
