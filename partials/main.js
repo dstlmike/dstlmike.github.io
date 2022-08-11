98a7d67bdc4e6dc3158616242ab9188b4f86510b
dstlmike <17973597+dstlmike@users.noreply.github.com>
2022-08-11 03:32:22 -0400

    Update main.js

--- partials/main.js
+++ partials/main.js
@@ -170,7 +170,7 @@ botText.innerText = " ";
   messagesContainer.appendChild(botDiv);
  
   setTimeout(() => {
-  botText.innerText = "\U+0042 \u+006F \u0074 \A Typing...";
+  botText.innerText = "\u0042 \u006F \u0074 \A Typing...";
 
   messagesContainer.scrollTop =
     messagesContainer.scrollHeight - messagesContainer.clientHeight;
