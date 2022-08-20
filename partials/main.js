062eecff38d922d10c815a13c3e589f5a5ea8a66
dstlmike <17973597+dstlmike@users.noreply.github.com>
2022-08-20 02:46:09 -0400

    Update main.js

--- partials/main.js
+++ partials/main.js
@@ -157,7 +157,7 @@ bttn.onclick = bttnInputtt();
   botDiv.className = "bot response";
   botText.innerText = " ";
   botDiv.appendChild(botText);
-
+messagesContainer.appendChild(botDiv);
 messagesContainer.scrollTop = messagesContainer.scrollHeight - messagesContainer.clientHeight;
 
 //document.body.appendChild(bttn); 
@@ -166,7 +166,7 @@ if (product == "Youuu") { //\u0041\u0067\u0065\u006E\u0074\u003A\u000DYouuu\u000
 //bttnInputt.onclick = function () { 
 //alert("Button is clicked"); 
 //}; 
-messagesContainer.appendChild(botDiv);
+
   setTimeout(() => {
   botText.innerText = "\u0041\u0067\u0065\u006E\u0074\u003A\u000D\u0054\u0079\u0070\u0069\u006E\u0067\u002E\u002E\u002E\u000D\u000D";
   messagesContainer.scrollTop = messagesContainer.scrollHeight - messagesContainer.clientHeight;
