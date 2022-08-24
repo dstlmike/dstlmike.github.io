c55930f46778f73e78560ad20e9754100aa851a1
dstlmike <17973597+dstlmike@users.noreply.github.com>
2022-08-24 04:09:18 -0400

    Update mainn.js

--- partials/mainn.js
+++ partials/mainn.js
@@ -61,6 +61,7 @@ console.log(`${name1}, ${name2}`);
 
 var utterances = [
 ["youu"],
+["youuu"],
 ["\u0068\u006F\u0077\u0020\u0061\u0072\u0065\u0020\u0079\u006F\u0075", "\u0068\u006F\u0077\u0020\u0069\u0073\u0020\u006C\u0069\u0066\u0065", "\u0068\u006F\u0077\u0020\u0061\u0072\u0065\u0020\u0074\u0068\u0069\u006E\u0067\u0073"],
   ["\u0068\u0069", "\u0068\u0065\u0079", "\u0068\u0065\u006C\u006C\u006F", "\u0067\u006F\u006F\u0064\u0020\u006D\u006F\u0072\u006E\u0069\u006E\u0067", "\u0067\u006F\u006F\u0064\u0020\u0061\u0066\u0074\u0065\u0072\u006E\u006F\u006F\u006E", "\u0067\u006F\u006F\u0064\u0020\u0065\u0076\u0065\u006E\u0069\u006E\u0067", "\u0068\u0069\u0020\u0074\u0068\u0065\u0072\u0065"],
   ["\u0077\u0068\u0061\u0074\u0020\u0061\u0072\u0065\u0020\u0079\u006F\u0075\u0020\u0064\u006F\u0069\u006E\u0067", "\u0077\u0068\u0061\u0074\u0020\u0069\u0073\u0020\u006E\u0065\u0077", "\u0077\u0068\u0061\u0074\u0020\u0069\u0073\u0020\u0067\u006F\u0069\u006E\u0067\u0020\u006F\u006E", "\u0077\u0068\u0061\u0074\u0020\u0069\u0073\u0020\u0075\u0070"],
@@ -75,6 +76,7 @@ var utterances = [
 
 var answers = [
 ["Youuu"],
+["Youuuu"],
 ["Fine... Thanks for asking.", "Pretty well, how can I help you?", "Fantastic, how can I assist?"],
   ["Hello!", "Hi!", "Hey!", "Hi there!", "Greetings!"],
   ["Nothing much.", "Improving customer experience, how can I be of help?", "I don't know actually."],
@@ -249,7 +251,7 @@ function handleClick() {
 if (product == "Youuu") { //\u0041\u0067\u0065\u006E\u0074\u003A\u000DYouuu\u000D\u000D") {
 messagesContainer.appendChild(botDiv);
  
-bttn.innerHTML = '<button id="bttnn" type="button" onclick="window.location.href=\'/support\'">Submit Itt</button>';
+bttn.innerHTML = `<button id="bttnn" type="button" onclick="window.location.href='/support'">Submit Itt</button>`;
 setTimeout(() => {
   botText.innerText = "\u0041\u0067\u0065\u006E\u0074\u003A\u000D\u0054\u0079\u0070\u0069\u006E\u0067\u002E\u002E\u002E\u000D\u000D";
   messagesContainer.scrollTop = messagesContainer.scrollHeight - messagesContainer.clientHeight;
@@ -266,7 +268,7 @@ messagesContainer.scrollTop = messagesContainer.scrollHeight - messagesContainer
 } else if (product == "Youuuu") {
 messagesContainer.appendChild(botDiv);
  
-bttn.innerHTML = '<button id="bttnn" type="button" onclick="window.location.href=\'https://marketbotai.com/support\'">Submit Itt</button>';
+bttn.innerHTML = `<button id="bttnn" type="button" onclick="window.location.href='https://marketbotai.com/support'">Submit Itt</button>`;
 setTimeout(() => {
   botText.innerText = "\u0041\u0067\u0065\u006E\u0074\u003A\u000D\u0054\u0079\u0070\u0069\u006E\u0067\u002E\u002E\u002E\u000D\u000D";
   messagesContainer.scrollTop = messagesContainer.scrollHeight - messagesContainer.clientHeight;
