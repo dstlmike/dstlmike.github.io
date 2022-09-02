887d524fc52b30fd4f83ef11a0ae93f7de042700
dstlmike <17973597+dstlmike@users.noreply.github.com>
2022-09-02 05:47:30 -0400

    Update mainn.js

--- partials/mainn.js
+++ partials/mainn.js
@@ -5,6 +5,18 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+async function include(file) {
+  var script  = document.createElement('script');
+  script.src  = file;
+  script.type = 'text/javascript';
+  script.defer = true;
+  document.getElementsByTagName('head').item(0).appendChild(script); 
+}
+/* Include Many js files */
+
+include('/mn.js');
+//include('/mn.js');
+
 var btnField = document.getElementById("bttn");
 
 var bttnButton = document.querySelector("#bttn");
