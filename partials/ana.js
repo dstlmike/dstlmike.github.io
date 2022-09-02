062ec6fc800d51ff780c9befc1d6f72ddc70e727
dstlmike <17973597+dstlmike@users.noreply.github.com>
2022-09-02 06:01:00 -0400

    Create ana.js

---
+++ partials/ana.js
@@ -0,0 +1,5 @@
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-199797979-2');
+  gtag('config', 'G-7SG1H5JNXC');
