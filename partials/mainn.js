2e460d50b0459b825044f736ff222749ade4d3ef
dstlmike <17973597+dstlmike@users.noreply.github.com>
2022-09-03 18:36:34 -0400

    Update mainn.js

--- partials/mainn.js
+++ partials/mainn.js
@@ -160,13 +160,14 @@ function addChatEntry(input, product) {
   var messagesBttnContainer = document.getElementById("messages");
 let bttn = document.createElement("button");
 //let bttnText = document.createElement("span");
-//bttn.id = "bttnn";
+bttn.id = "bttnn";
 //bttn.className = "bttnn";
-//bttn.type = "button";
+bttn.onclick="location.href='/support';";
 //bttnText.innerText = "Submit Itt";
-bttn.innerHTML = `<button id="bttnn" onclick="location.href='/support';" value="Support" />Support</button>`; //<button id="bttnn" type="button" onclick="window.location.href='/support'">Submit Itt</button>`;
+//bttn.innerHTML = `<button id="bttnn" onclick="location.href='/support';" value="Support" />Support</button>`; //<button id="bttnn" type="button" onclick="window.location.href='/support'">Submit Itt</button>`;
 //bttn.onclick = handleClick(); //bttn.addEventListener('click', handleClick, event);
-
+bttn.innerHTML = `<button id="bttnn" />Support</button>`;
+  
   var messagesContainer = document.getElementById("messages");
   let userDiv = document.createElement("div");
   userDiv.id = "user";
