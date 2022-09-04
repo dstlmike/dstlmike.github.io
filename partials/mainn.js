2564379869e5e78a14f361dfa6c2252529d99aa7
dstlmike <17973597+dstlmike@users.noreply.github.com>
2022-09-04 00:11:22 -0400

    Update mainn.js

--- partials/mainn.js
+++ partials/mainn.js
@@ -172,10 +172,10 @@ let bttn = document.createElement("button");
 //var bttnText = document.querySelector("bttnn");
 bttn.id = "bttnn";
 //bttn.addEventListener("click", function() {location.href='/support' };
-//bttn.className = "bttnn";
-//bttn.onclick = "location.href='/support'";
+bttn.className = "bttnn";
+bttn.onclick = "window.location.href='https://chat-chat.b9ad.pro-us-east-1.openshiftapps.com/support'";
 //bttnText.innerText = "Submit Itt";
-bttn.innerHTML = `<button id="bttnn" type="button" onclick="window.location.href='https://chat-chat.b9ad.pro-us-east-1.openshiftapps.com';">Support</button>`; //<button id="bttnn" type="button" onclick="window.location.href='/support'">Submit Itt</button>`;
+bttn.innerHTML = `Support`; //<button id="bttnn" type="button" onclick="window.location.href='/support'">Submit Itt</button>`;
 //bttn.onclick = handleClick(); //bttn.addEventListener('click', handleClick, event);
 //bttn.innerHTML = `<button id="bttnn" />Support</button>`;
   
