12506c48df05e6cf2c940e2a836cf8abdc5f5f96
dstlmike <17973597+dstlmike@users.noreply.github.com>
2022-09-04 02:43:50 -0400

    Update mn.js

--- partials/mn.js
+++ partials/mn.js
@@ -7,7 +7,7 @@ var submitIn = function(event) {
 
 
 let input = inputField.value;
-if (event.code === 13 && inputField.value && inputtt == "false") {
+if (inputField.value && inputtt == "false") {
 inputField.value = "";
      output(input);
  stateHandle;
