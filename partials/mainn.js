91e53641cfc0809db47036b7a442a247c10ce4c2
dstlmike <17973597+dstlmike@users.noreply.github.com>
2022-09-04 23:47:18 -0400

    Update mainn.js

--- partials/mainn.js
+++ partials/mainn.js
@@ -296,7 +296,7 @@ messagesContainer.scrollTop = messagesContainer.scrollHeight - messagesContainer
 
 function openForm() {
    document.getElementById("myForm").style.display = "block";
-  inputField.autofocus = true;
+//  inputField.autofocus = true;
   var box = document.getElementById('messages');
 if (box.childNodes.length === 0) {
   addChatEntryBot();
