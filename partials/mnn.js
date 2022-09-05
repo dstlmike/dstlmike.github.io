ce68a2228bbfce8a020fb745b379350929dcdbeb
dstlmike <17973597+dstlmike@users.noreply.github.com>
2022-09-05 00:13:20 -0400

    Update mnn.js

--- partials/mnn.js
+++ partials/mnn.js
@@ -2,13 +2,13 @@
 //var inputtt = require('./mainn.js');
 //var inputt = require('./mainn.js'); 
 var stateHandle = function() {
-         inputField.autofocus = true;
+        // inputField.autofocus = true;
  inputtt = "true";
         inputt.disabled = true;
      setTimeout(() => {
        inputtt = "false";
         inputt.disabled = false;
-      inputField.autofocus = true;
+   //   inputField.autofocus = true;
       }, 6000);
 }
 module.exports = stateHandle;
