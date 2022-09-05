a7bdb29cd2835357a722d1bacba3435709394d87
dstlmike <17973597+dstlmike@users.noreply.github.com>
2022-09-05 00:15:14 -0400

    Update mn.js

--- partials/mn.js
+++ partials/mn.js
@@ -11,7 +11,7 @@ if (inputField.value) { // && inputtt == "false") {
 
 // if (inputtt == false)
 inputField.value = "";
-
+inputField.autofocus = true;
      output(input);
  stateHandle();
 //event.preventDefault();
