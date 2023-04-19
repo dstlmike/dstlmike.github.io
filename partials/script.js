135923d71ae68f842531f7c51a50c27c139466cd
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-04-19 05:38:52 -0400

    Update script.js

--- partials/script.js
+++ partials/script.js
@@ -1,9 +1,9 @@
 function parallax_height() {
-  var scroll_bottom = $(this).scrollBottom();
-  var sample_section_bottom = $(".sample-section").offset().bottom;
+  var scroll_top = $(this).scrollTop();
+  var sample_section_top = $(".sample-section").offset().top;
   var header_height = $(".sample-header-section").outerHeight();
-  $(".sample-section").css({ "margin-bottom": header_height });
-  $(".sample-header").css({ height: header_height - scroll_bottom });
+  $(".sample-section").css({ "margin-top": header_height });
+  $(".sample-header").css({ height: header_height - scroll_top });
 }
 parallax_height();
 $(window).scroll(function() {
