ac100cc5122f3ff72632c65097641420a7165a07
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-04-21 00:15:55 -0400

    Create script.js

---
+++ partials/script.js
@@ -0,0 +1,20 @@
+gsap.utils.toArray(".parallax").forEach((section, i) => {
+  section.bg = section.querySelector(".bg"); 
+
+  // Give the backgrounds some random images
+  section.bg.style.backgroundImage = `url(https://picsum.photos/${innerWidth}/${innerHeight}?random=${i})`;
+
+  // Do the parallax effect on each section
+  if (i) {
+    section.bg.style.backgroundPosition = `50% ${innerHeight / 2}px`;
+
+    gsap.to(section.bg, {
+      backgroundPosition: `50% ${-innerHeight / 2}px`,
+      ease: "none",
+      scrollTrigger: {
+        trigger: section,
+        scrub: true
+      }
+    });
+  } 
+});
