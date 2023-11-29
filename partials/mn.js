f16aa78e7a253d33c3b4950451fdb91359327943
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-11-29 16:24:33 -0500

    Update mn.js

--- partials/mn.js
+++ partials/mn.js
@@ -1,7 +1,7 @@
 //var inputtt = "false"; // require("./main.js");
-var inputField = require('./partials/mainn.js');
-var stateHandle = require('./partials/mnn.js');
-var outputt = require('./partials/mainn.js');
+//var inputField = require('./partials/mainn.js');
+//var stateHandle = require('./partials/mnn.js');
+//var outputt = require('./partials/mainn.js');
 
 var submitIn = function() {
 let input = inputField.value;
